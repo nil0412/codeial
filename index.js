4cb3e4b65918902a76a6d242433b454598f2da5b
=nil0412 <nil041297@gmail.com>
2022-12-28 00:34:34 +0530

    Add signUP Page

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = 8000;
+
+app.use(express.urlencoded());
+
+app.use(cookieParser());
 
 const expressLayouts = require('express-ejs-layouts');
 app.use(expressLayouts);
