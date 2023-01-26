6b7593ce6f951e98aeb883c22dc5456b0c1d684c
Nilesh Patil <nil041297@gmail.com>
2023-01-26 15:40:48 +0530

    added mailer and started with delay task using kue and redis

---
+++ config/nodemailer.js
@@ -0,0 +1,37 @@
+const nodemailer = require('nodemailer');
+const ejs = require('ejs');
+const path = require('path');
+
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    auth: {
+      user: 'donotreply.codeial@gmail.com', // generated ethereal user
+      pass: 'dymszymmorbbamvi', // generated ethereal password
+    },
+});
+
+let renderTemplate = (data, relativePath) => {
+    let mailHTML;
+    ejs.renderFile(
+        path.join(__dirname, '../views/mailers', relativePath),
+        data,
+        function(err, template){
+            if(err){
+                console.log("Error in rendering template", err);
+                return;
+            }
+
+            mailHTML = template;
+        }
+    )
+    return mailHTML;
+}
+
+
+module.exports = {
+    transporter: transporter,
+    renderTemplate: renderTemplate
+}
