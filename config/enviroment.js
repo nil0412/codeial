a4dbee092afe2d7379cad1794d77c802de77dd68
Nilesh Patil <nil041297@gmail.com>
2023-01-29 20:23:29 +0530

    Updated node to v14.17.3 as getting error for fs/promises

--- config/enviroment.js
+++ config/enviroment.js
@@ -5,7 +5,7 @@ const path = require('path');
 const logDirectory = path.join(__dirname, '../production_logs');
 fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
 
-const accessLogStream = rfs('access.log', {
+const accessLogStream = rfs.createStream('access.log', {
   interval: '1d',
   path: logDirectory
 });
