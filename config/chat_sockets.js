6e0e3ce5ece8c1a6e6587397b4cf26fad4f13a2f
Nilesh Patil <nil041297@gmail.com>
2023-02-06 21:00:30 +0530

    minor changes

--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -1,11 +1,11 @@
 
 module.exports.chatSockets = function(socketServer){
     let io = require('socket.io')(socketServer, {
-        cors: {
-          origin: "http://localhost:8000",
-        //   origin: "http://100.25.143.16:8000",
-          methods: ["GET", "POST"]
-        }
+        // cors: {
+        //   origin: "http://localhost:8000",
+        // //   origin: "http://100.25.143.16:8000",
+        //   methods: ["GET", "POST"]
+        // }
       });
 
     io.sockets.on('connection', function(socket){
